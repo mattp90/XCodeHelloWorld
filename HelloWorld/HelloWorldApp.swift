2cef50b0df779c0f73263ba0d7d52da72c594a1b
Matteo Piazzi <piazzi.matteo@gmail.com>
2022-12-08 20:12:36 +0100

    this is the real first commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Matteo Piazzi on 08/12/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
